ef65f97d7e996457b04a6de974e9083895c0c6dc
sertemo <tejedor.moreno@gmail.com>
2024-04-05 14:10:03 +0200

    modificado .check.sh para que verifique en todo el directorio

--- .check.sh
+++ .check.sh
@@ -22,15 +22,15 @@ NC='\033[0m' # Sin color
 project_path="src"
 
 echo -e "${YELLOW}Ejecutando Black...${NC}"
-poetry run black "$project_path"
+poetry run black .
 echo -e "${GREEN}Black finalizado correctamente.${NC}"
 
 echo -e "${YELLOW}Ejecutando MyPy...${NC}"
-poetry run mypy "$project_path"
+poetry run mypy .
 echo -e "${GREEN}MyPy finalizado correctamente.${NC}"
 
 echo -e "${YELLOW}Ejecutando Flake8...${NC}"
-if poetry run flake8 "$project_path"; then
+if poetry run flake8 .; then
     echo -e "${GREEN}Flake8 finalizado correctamente. No se encontraron problemas.${NC}"
 else
     echo -e "${RED}Flake8 encontró problemas.${NC}"
@@ -40,6 +40,4 @@ echo -e "${YELLOW}Ejecutando Pytest...${NC}"
 poetry run pytest
 echo -e "${GREEN}Pytest finalizado correctamente.${NC}"
 
-# ejecutar chmod +x tests.sh para hacer el archivo ejecutable: ./tests.sh
-# esto lo hace automáticamente el archivo create.sh
 
